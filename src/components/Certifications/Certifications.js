240f00f72e748241c83ffe6c1c2f22157ad3a53e
kikepola <kikepola@gmail.com>
2021-04-13 22:36:56 -0300

    Updating experiences and certifications

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './Certifications.css';
 import LetsCode from './../../assets/img/lets_code.png';
 import SCRUMstudy from './../../assets/img/SCRUMStudy.png'
-
+import Alura from './../../assets/img/alura.jpg';
+import Descola from './../../assets/img/descola.jpg';
 
 export default function Certifications(){
     return(
@@ -23,7 +24,55 @@ export default function Certifications(){
                     <p className="CertificationTitle">SCRUMstudy - Accreditation Body for Scrum and Agile</p>
                     <p className="CertificationYear">Issued May 2019</p>
                 </div>
-            </div>        
+            </div>    
+            <div className="Certification">
+                <img src={Alura} className="SCRUMstudyLogo" alt="SCRUMstudyLogo"/>
+                <div className="CertificationContent">
+                    <p className="CertificationInstituteName">Alura</p>
+                    
+                    <p className="CertificationTitle">Design Patterns C# II: Boas práticas de programação</p>
+                    <p className="CertificationYear">Issued Mar 2021</p>
+                    
+                    <p className="CertificationTitle">Design Patterns C# I: Boas práticas de programação</p>
+                    <p className="CertificationYear">Issued Mar 2021</p>
+                    
+                    <p className="CertificationTitle">SOLID com C#: Princípios da programação orientada a objetos</p>
+                    <p className="CertificationYear">Issued Mar 2021</p>
+                    
+                    <p className="CertificationTitle">Kafka: Produtores, Consumidores e streams</p>
+                    <p className="CertificationYear">Issued Jan 2021</p>
+                    
+                    <p className="CertificationTitle">NodeJS: Avançando em APIs REST com controle de versões</p>
+                    <p className="CertificationYear">Issued Jan 2021</p>
+
+                    <p className="CertificationTitle">NodeJS: Crie uma API REST padronizada e escalável</p>
+                    <p className="CertificationYear">Issued Jan 2021</p>
+
+                    <p className="CertificationTitle">NodeJS: Streaming de dados e Repositório</p>
+                    <p className="CertificationYear">Issued Jan 2021</p>
+
+                    <p className="CertificationTitle">ORM com NodeJS: API com Sequelize e MySQL</p>
+                    <p className="CertificationYear">Issued Jan 2021</p>
+
+                    <p className="CertificationTitle">HTTP: Entendendo a web por baixo dos panos</p>
+                    <p className="CertificationYear">Issued Dec 2020</p>
+
+                    <p className="CertificationTitle">Rest com NodeJS: API com Express e MySQL</p>
+                    <p className="CertificationYear">Issued Dec 2020</p>
+                </div>
+            </div>  
+            <div className="Certification">
+                <img src={Descola} className="SCRUMstudyLogo" alt="SCRUMstudyLogo"/>
+                <div className="CertificationContent">
+                    <p className="CertificationInstituteName">Descola</p>
+                    
+                    <p className="CertificationTitle">Mentalidade Ágil: aprenda a planejar, priorizar e executar usando modelos ágeis</p>
+                    <p className="CertificationYear">Issued Dec 2020</p>
+                    
+                    <p className="CertificationTitle">SQUAD: gestão ágil para times autogerenciáveis e multidisciplinares</p>
+                    <p className="CertificationYear">Issued Dec 2020</p>                                        
+                </div>
+            </div>   
         </div>
     );
 }
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,17 +1,26 @@
 import React from  'react';
 import './Experience.css'
 import ItauLogo from './../../assets/img/itau.png';
+import ArgoLogo from './../../assets/img/argo.png';
 
 export default function Experience(){
     return(
         <div className="Experience">
             <h2>Experience</h2>
-            <div className="Itau">
-                <img src={ItauLogo} className="ItauLogo" alt="ItauLogo"/>
+            <div className="ExperienceCard">
+                <img src={ArgoLogo} className="ExperienceLogo" alt="ArgoLogo"/>
+                <div className="ExperienceContent">
+                    <p className="ExperienceInstituteName">Argo Solutions</p>
+                    <p className="ExperienceTitle">Full Stack Developer</p>
+                    <p className="ExperienceYear">Dec 2020 – Present</p>
+                </div>                
+            </div>  
+            <div className="ExperienceCard">
+                <img src={ItauLogo} className="ExperienceLogo" alt="ItauLogo"/>
                 <div className="ExperienceContent">
                     <p className="ExperienceInstituteName">Itaú Unibanco</p>
                     <p className="ExperienceTitle">JR Operational Finance Analyst</p>
-                    <p className="ExperienceYear">Aug 2019 – Present</p>
+                    <p className="ExperienceYear">Aug 2019 – Dec 2020</p>
                     <p className="ExperienceTitle">Internship</p>
                     <p className="ExperienceYear">Oct 2018 – Aug 2019</p>
                 </div>                
